c6f5b0f2c79c3eb619df7145fd05419c449e255b
vcxiz <32043469+vcxiz@users.noreply.github.com>
2017-09-19 17:53:26 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,33 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
+// Embed function
+function embedMessage(title, description = 'ᅠ') => {
+
+  if (description !== 'ᅠ') description += '\n';
+
+  return {
+
+    title,
+
+    description: `${description}\n[Better processing for Babu? Donate today!](link unavailable)`,
+
+    timestamp: new Date(),
+
+    footer: {
+
+      text: 'Powered by Babu',
+
+      icon_url: 'https://i.imgur.com/tQ3brof.png'
+
+    }
+
+  }
+
+}
+
+
+
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -20,10 +47,11 @@ client.on('message', message => {
   // If the message is "ping"
   if (message.content === 'b!ping') {
     // Send "pong" to the same channel
-    message.channel.send('Pong!');
+    message.channel.send({embed: embedMessage("Output", "Pong!")});
   }
    if (message.content === 'b!help') {
-     message.author.sendMessage("Help currently WIP!")
+    
+     message.author.sendMessage({embed: embedMessage("Babu Help", "Help is currently in progress. Try again later.")})
    }
 });
 // THIS  MUST  BE  THIS  WAY
